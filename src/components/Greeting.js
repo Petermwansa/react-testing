3791e1a44fca8380010232cbb65445b19e13e06e
Petermwansa <mwansapeter800@gmail.com>
2024-08-19 13:42:12 +0300

    Add the first greeting text and remove the app.test file

---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Greeting = () => {
+  return (
+    <div>
+      <h2>Hello Peter!</h2>
+      <p>I love React</p>
+    </div>
+  )
+}
+
+export default Greeting
---
+++ src/components/Greeting.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react"
+import Greeting from "./Greeting"
+
+test('renders Hello Peter as a text', () => {
+    //  Arrange 
+    render(<Greeting />);
+
+    
+    // Act 
+    // ...nothing 
+
+
+
+    // Assert 
+    const helloWorldElement = screen.getByText('Hello Peter', { exact: false});
+    expect(helloWorldElement).toBeInTheDocument();
+})
