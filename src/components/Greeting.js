6343e7d0682d37a95536f52050144fc6fe7e974b
Petermwansa <mwansapeter800@gmail.com>
2024-08-19 14:09:11 +0300

    Add a third test to to the testing after the click of a button

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Greeting = () => {
+    const [changedText, setChangedtext] = useState(false);
+
+    const changetextHandler = () => {
+        setChangedtext(true);
+    }
+
   return (
     <div>
       <h2>Hello Peter!</h2>
-      <p>I love React</p>
+      {!changedText && <p>I love React</p>}
+      {changedText && <p>This was clicked</p>}
+      <button onClick={changetextHandler}>Change Text</button>
     </div>
   )
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,17 +1,69 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
+import userEvent from "@testing-library/user-event";
 
-test('renders Hello Peter as a text', () => {
-    //  Arrange 
-    render(<Greeting />);
-
+describe("Greeting component", () => {
+    test('renders Hello Peter as a text', () => {
+        //  Arrange 
+        render(<Greeting />);
     
-    // Act 
-    // ...nothing 
+    
+        // Act 
+        // ...nothing 
+    
+    
+    
+        // Assert 
+        const helloWorldElement = screen.getByText('Hello Peter', { exact: false});
+        expect(helloWorldElement).toBeInTheDocument();
+    });
 
 
+    test('renders I love react as a test if the button was not cliked', () => {
+        //  Arrange 
+        render(<Greeting />);
+    
+    
+        // Act 
+        // ...nothing 
+    
+    
+    
+        // Assert 
+        const reactLoveElement = screen.getByText('I love React');
+        expect(reactLoveElement).toBeInTheDocument();
+    });
 
-    // Assert 
-    const helloWorldElement = screen.getByText('Hello Peter', { exact: false});
-    expect(helloWorldElement).toBeInTheDocument();
+
+    test('render changed if the button was clicked', () => {
+        // Arrange 
+        render(<Greeting />);
+
+
+        // Act 
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+
+        // Assert 
+        const outputElement = screen.getByText('This was clicked');
+        expect(outputElement).toBeInTheDocument();
+    })
+
+
+    test('render no first paragraph with the text: I love react. after the button was clicked', () => {
+        // Arrange 
+        render(<Greeting />);
+
+
+        // Act 
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+
+        // Asserting 
+        const outputElement = screen.queryByText('I love React');
+        expect(outputElement).toBeNull();
+
+    })
 })
